16dd0527d78cbe628c0de1d1e6cb960bdac2ab91
nigloo <slaxalde@enseirb-matmeca.fr>
2014-04-04 17:35:36 +0900

    Refactor + color handling + type checking

--- d4.js
+++ d4.js
@@ -12,45 +12,55 @@
   var linear_scale_padding = 0.1
   
   
-  // definiting the base constructor for all classes, which will execute the final class prototype's initialize method if exists
+  // Definiting the base constructor for all classes, which will execute the final class prototype's initialize method if exists
   var Class = function() {
     this.initialize && this.initialize.apply(this, arguments);
   };
-  Class.extend = function(childPrototype) { // defining a static method 'extend'
+  Class.extend = function(childPrototype) { // Defining a static method 'extend'
     var parent = this;
-    var child = function() { // the child constructor is a call to its parent's
+    var child = function() { // The child constructor is a call to its parent's
       return parent.apply(this, arguments);
     };
-    child.extend = parent.extend; // adding the extend method to the child class
-    var Surrogate = function() {}; // surrogate "trick
+    child.extend = parent.extend; // Adding the extend method to the child class
+    var Surrogate = function() {}; // Surrogate "trick"
     Surrogate.prototype = parent.prototype;
     child.prototype = new Surrogate;
     for(var key in childPrototype){
         child.prototype[key] = childPrototype[key];
     }
-    return child; // returning the child class
+    return child; // Returning the child class
   };
   
-  // TODO : attributes -> {stroke_width:"x", type:number}
-  // TODO : x, y -> position (array of dimentions)
+  // TODO : x, y -> position (array of dimentions)m,
   
+  // Elements definition
+  // Remark: dimention can be both numerical value or string (discrete value)
   var Element = Class.extend({
     initialize : function() {
-      this.x = 0;
-      this.y = 0;
-      this.color = 'white';
-      this.stroke_width = 1;
-      this.stroke_color = 'black';
+      this.name = 'Element';
+      this.x = {type:'dimention',
+                value:0};
+      this.y = {type:'dimention',
+                value:0};
+      this.fill = {type:'color',
+                    value:'white'};
+      this.stroke_width = {type:'number',
+                           value:1};
+      this.stroke = {type:'color',
+                           value:'black'};
      }
   });
     
   var Circle = Element.extend({
     initialize : function() {
-      this.radius = 5;
+      this.name = 'Circle';
+      this.radius = {type:'number',
+                     value:5};
      }
   });
   
   
+  // Graphic definition
   window.Graphic = function() {
     this.dataset = null;          
     this.elements = new Array();
@@ -60,14 +70,16 @@
   
   // Set element properties
   Graphic.prototype.element = function(param) {
+    this.fallback_element = new Element();
+    
     if(typeof param != 'undefined') {
       for(var attr in this.fallback_element) {
         if(typeof param[attr] != 'undefined') {
-          this.fallback_element[attr] = param[attr];
+          this.fallback_element[attr].value = param[attr];
         }
       }
     }
-    //console.log(this.fallback_element);
+
     return this;
   }
   
@@ -77,13 +89,16 @@
     
     // copying attributes' values from the fallback element
     for(var attr in this.fallback_element) {
-      circle[attr] = this.fallback_element[attr];
+      if(typeof this.fallback_element[attr].type != 'undefined') {
+        circle[attr] = this.fallback_element[attr];
+      }
     }
     
     if(typeof param != 'undefined') {
-      for(attr in circle) {
-        if(typeof param[attr] != 'undefined') {
-          circle[attr] = param[attr];
+      for(var attr in circle) {
+        if(typeof param[attr] != 'undefined' &&
+           typeof circle[attr] != 'undefined' && typeof circle[attr].type != 'undefined') {
+          circle[attr].value = param[attr];
         }
       }
     }
@@ -99,45 +114,8 @@
     
     return this;
   }
-  
-  function addPadding(interval, padding) {
-    return [interval[0] - (interval[1] - interval[0]) * padding,
-            interval[1] + (interval[1] - interval[0]) * padding];
-  }
-  
-  
-  // Private : compute some stats (min and max only for now)
-  function computeStat(dataset, critera) {
-    var f;
-    
-    if(typeof critera === 'function') {
-      f = critera;
-    }
-    else { // critera is a column / variable / aestetic
-      f = function (d) {return d[critera];}
-    }
-    
-    
-    
-    var min = f(dataset[0], 0),
-        max = min;
-    
-    for(var i = 1 ; i < dataset.length ; i++){
-      var val = f(dataset[i], i);
-      
-      if(val < min) {
-        min = val;
-      }
-      else if(val > max) {
-        max = val;
-      }
-    }
-    
-    return {min:min, max:max}
-  }
-  
-  
-  // Genere le svg
+
+  // Render the graphic in svg
   Graphic.prototype.render = function(param) {
     if(this.dataset == null) {
       throw 'No dataset';
@@ -185,14 +163,7 @@
                   max:width - margin.right},
                y:{min:height - margin.bottom,
                   max:margin.top}}
-
-
-    /*
-    console.log(selector);
-    console.log(width);
-    console.log(heigth);
-    console.log(computeStat(this.dataset, "", function(d){return -1*d.col1;}));
-    //*/
+    
     
     var svg = d3.select(selector)
                 .append("svg")
@@ -203,112 +174,31 @@
     // Statitics on each column / variable / aestetic
     var stats = {};
     
-    // scale for each dimention (x & y for now)
+    // Scale for each dimention (x & y for now)
     var scales = {};
     
     for(var i = 0 ; i < this.elements.length ; i++) {
       for(var attr in this.elements[i]) {
-        var attr_val = this.elements[i][attr];
+        // Uninteresting attributes (initialise, name, ...)
+        if(typeof this.elements[i][attr].type === 'undefined') {
+          continue;
+        }
+        
+        var attr_type = this.elements[i][attr].type;
+        var attr_val = this.elements[i][attr].value;
         
         // If the attribute is bind to an aestetic
         if(typeof attr_val === 'string' && attr_val.indexOf(data_binding_prefix) == 0) {
           var column = attr_val.substring(data_binding_prefix.length);
           
-          if(attr == 'x' || attr == 'y') {
-            // Computing the scale
-            if(typeof this.dataset[0][column] == 'number') {
-              if(typeof stats[column] === 'undefined') {
-                stats[column] = computeStat(this.dataset, column)
-              }
-                
-              scales[attr] = d3.scale.linear()
-                               .domain(addPadding([stats[column].min, stats[column].max], linear_scale_padding))
-                               .range([lim[attr].min, lim[attr].max])
-                               .nice();
-            }
-            else { // bind attribute on x or y but not number
-              var domain = new Array();
-              
-              for(var j = 0 ; j < this.dataset ; j++) {
-                if(domain.indexOf(this.dataset[j][column]) == -1) {
-                  domain.push(this.dataset[j][column]);
-                }
-              }
-              
-              scales[attr] = d3.scale.ordinal()
-                               .domain(domain)
-                               .rangePoints([lim[attr].min, lim[attr].max], ordinal_scale_padding);
-            }
-            
-            var Closure = function (s) {
-                this.scale = s;
-                // capture du contexte
-                var me = this;
-                return {
-                    action:function (d) {
-                        return me.scale(d[column]);
-                    }
-                }
-            };
-            this.elements[i][attr] = (new Closure(scales[attr])).action;
-          }
-          else { // bind to an aestetic but not a dimentions
-            this.elements[i][attr] = function (d) {
-              return d[column];
-            }
-          }
+          // We convert it into a fonction
+          this.elements[i][attr].value = function (d, i) {return d[column]}
         }
-        else if(typeof attr_val === 'function') { // If the value of the attribute is computed by a function
-          if(attr == 'x' || attr == 'y') {
-            var func = attr_val; // Just to make it clearer
-            
-            // Computing the scale
-            if(typeof func(this.dataset[0]) == 'number') {
-              if(typeof stats[func] === 'undefined') {
-                stats[func] = computeStat(this.dataset, func)
-              }
-              
-              scales[attr] = d3.scale.linear()
-                               .domain(addPadding([stats[func].min, stats[func].max], linear_scale_padding))
-                               .range([lim[attr].min, lim[attr].max])
-                               .nice();
-              
-            }
-            else {
-              var domain = new Array();
-              
-              for(var j = 0 ; j < this.dataset ; j++) {
-                var value = func(this.dataset[j]);
-                
-                if(domain.indexOf(value) == -1) {
-                  domain.push(value);
-                }
-              }
-              
-              scales[attr] = d3.scale.ordinal()
-                               .domain(domain)
-                               .rangePoints([lim[attr].min, lim[attr].max], ordinal_scale_padding);
-            }
-            
-            var Closure = function (s) {
-                this.scale = s;
-                // capture du contexte
-                var me = this;
-                return {
-                    action:function (d) {
-                        return me.scale(func(d));
-                    }
-                }
-            };
-            this.elements[i][attr] = (new Closure(scales[attr])).action;
-          }
-          // If the value of the attribute is computed by a function and
-          // is not a dimention, nothing to do (no scaling)
-        }
-        else { // If the value of the attribute is constant
-          if(attr == 'x' || attr == 'y') {
-            // Computing the scale
-            if(typeof attr_val == 'number') {
+        // If the value of the attribute is constant
+        else if(typeof attr_val === 'number' || typeof attr_val === 'string') {
+          // Computing the scale
+          if(attr_type === 'dimention' && typeof scales[attr] === 'undefined') {
+            if(typeof attr_val === 'number') {
               var domain;
               
               if(attr_val != 0) {
@@ -321,25 +211,113 @@
               scales[attr] = d3.scale.linear()
                                .domain(domain)
                                .range([lim[attr].min, lim[attr].max]);
+                               
             }
-            else {
+            else { // typeof attr_val === 'string'
               scales[attr] = d3.scale.ordinal()
                                .domain([attr_val])
-                               .range([lim[attr].min, lim[attr].max]);
+                               .rangePoints([lim[attr].min, lim[attr].max], ordinal_scale_padding);
             }
           }
           
           var Closure = function (v) {
-              this.value = v;
-              // capture du contexte
+            this.value = v;
+            var me = this;
+            return {
+              action:function (d, i) {
+                return me.value;
+              }
+            }
+          };
+          this.elements[i][attr].value = (new Closure(attr_val)).action;
+        }
+        else if(typeof attr_val != 'function') {
+          throw errorMessage(this.elements[i].name, attr, (typeof attr_val), attr_type);
+        }
+        
+        
+        // If the value of the attribute is computed by a function
+        var func = this.elements[i][attr].value; // Just to make it clearer
+        var func_ret_type = typeof func(this.dataset[0], 0);
+        
+        switch(attr_type) {
+          case 'dimention':
+            // Computing the scale
+            if(typeof scales[attr] === 'undefined') {
+              if(func_ret_type === 'number') {
+                if(typeof stats[func] === 'undefined') {
+                  stats[func] = computeStat(this.dataset, func)
+                }
+                
+                scales[attr] = d3.scale.linear()
+                                 .domain(addPadding([stats[func].min, stats[func].max], linear_scale_padding))
+                                 .range([lim[attr].min, lim[attr].max])
+                                 .nice();
+                
+              }
+              else if(func_ret_type === 'string') {
+                var domain = new Array();
+                
+                for(var row in this.dataset) {
+                  var value = func(row);
+                  
+                  if(domain.indexOf(value) == -1) {
+                    domain.push(value);
+                  }
+                }
+                
+                scales[attr] = d3.scale.ordinal()
+                                 .domain(domain)
+                                 .rangePoints([lim[attr].min, lim[attr].max], ordinal_scale_padding);
+              }
+              else {
+                throw errorMessage(this.elements[i].name, attr, (typeof attr_val), '\'number\' or \'string\'');
+              }
+            }
+            
+            var Closure = function (s, f) {
+              this.scale = s;
+              this.func = f;
               var me = this;
               return {
-                  action:function (d) {
-                      return me.value;
-                  }
+                action:function (d, i) {
+                  return me.scale(me.func(d, i));
+                }
               }
-          };
-          this.elements[i][attr] = (new Closure(attr_val)).action;
+            };
+            this.elements[i][attr].value = (new Closure(scales[attr], func)).action;
+            break;
+          
+          case 'color':
+            if(func_ret_type === 'number') {
+              if(typeof stats[func] === 'undefined') {
+                stats[func] = computeStat(this.dataset, func)
+              }
+              
+              var scale = d3.scale.category10()
+                        .domain([stats[func].min, stats[func].max]);
+                                               
+              var Closure = function (s, f) {
+                this.scale = s;
+                this.func = f;
+                var me = this;
+                return {
+                  action:function (d, i) {
+                    return me.scale(me.func(d, i));
+                  }
+                }
+              };
+              this.elements[i][attr].value = (new Closure(scale, func)).action;
+            }
+            else if(func_ret_type != 'string') {
+              throw errorMessage(this.elements[i].name, attr, (typeof attr_val), 'color (\'number\' or \'string\')');
+            }
+            break;
+          case 'number':
+            if(func_ret_type != 'number') {
+              throw errorMessage(this.elements[i].name, attr, (typeof attr_val), '\'number\'');
+            }
+            break;
         }
       }
       
@@ -349,11 +327,15 @@
            .data(this.dataset)
            .enter()
            .append("circle")
-           .attr("cx", this.elements[i].x)
-           .attr("cy", this.elements[i].y)
-           .attr("r", this.elements[i].radius);
+           .attr("cx", this.elements[i].x.value)
+           .attr("cy", this.elements[i].y.value)
+           .attr("r", this.elements[i].radius.value)
+           .attr("stroke-width", this.elements[i].stroke_width.value)
+           .attr("stroke", this.elements[i].stroke.value)
+           .attr("fill", this.elements[i].fill.value);
       }
     }
+    
     
     // X axis
     var xAxis = d3.svg.axis()
@@ -379,5 +361,56 @@
 
   }
   
+  
+  ///////////////////////
+  // Private functions //
+  ///////////////////////
+
+  function addPadding(interval, padding) {
+    if(interval[0] != interval[1]) {
+      return [interval[0] - (interval[1] - interval[0]) * padding,
+              interval[1] + (interval[1] - interval[0]) * padding];
+    }
+    else if(interval[0] != 0) {
+      return [0, interval[0] * 2];
+    }
+  }
+  
+  
+  // Private : compute some stats (min and max only for now)
+  function computeStat(dataset, critera) {
+    var f;
+    
+    if(typeof critera === 'function') {
+      f = critera;
+    }
+    else { // critera is a column / variable / aestetic
+      f = function (d) {return d[critera];}
+    }
+    
+    
+    
+    var min = f(dataset[0], 0),
+        max = min;
+    
+    for(var i = 1 ; i < dataset.length ; i++){
+      var val = f(dataset[i], i);
+      
+      if(val < min) {
+        min = val;
+      }
+      else if(val > max) {
+        max = val;
+      }
+    }
+    
+    return {min:min, max:max}
+  }
+  
+  
+  function errorMessage(elt_name, attribute, type, expected) {
+    return elt_name+'.'+attribute+' don\'t support value of type \''+type+
+           '\'; Expected: '+expected;
+  }
   
 }();
